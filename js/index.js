763c137453af717007a216b1af8d77318f64ca9b
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-07-20 13:17:56 +0900

    220720
    index js editing....

--- js/index.js
+++ js/index.js
@@ -145,34 +145,64 @@ $.ajax({
     });
 
 $(function () {
+    //genre
     $(".genre>button:nth-of-type(1)").click(function () {
         $(this).addClass("btn_on");
         $(".genre>button:nth-of-type(2), .genre>button:nth-of-type(3), .genre>button:nth-of-type(4), .genre>button:nth-of-type(5)").removeClass("btn_on");
-        $(".genre1").fadeIn(250);
+        $(".genre1").fadeIn(250).css("zIndex", "1");
         $(".genre2, .genre3, .genre4, .genre5").fadeOut(250);
     })
     $(".genre>button:nth-of-type(2)").click(function () {
         $(this).addClass("btn_on");
         $(".genre>button:nth-of-type(1), .genre>button:nth-of-type(3), .genre>button:nth-of-type(4), .genre>button:nth-of-type(5)").removeClass("btn_on");
-        $(".genre2").fadeIn(250);
+        $(".genre2").fadeIn(250).css("zIndex", "1");
         $(".genre1, .genre3, .genre4, .genre5").fadeOut(250);
     })
     $(".genre>button:nth-of-type(3)").click(function () {
         $(this).addClass("btn_on");
         $(".genre>button:nth-of-type(1), .genre>button:nth-of-type(2), .genre>button:nth-of-type(4), .genre>button:nth-of-type(5)").removeClass("btn_on");
-        $(".genre3").fadeIn(250);
+        $(".genre3").fadeIn(250).css("zIndex", "1");
         $(".genre1, .genre2, .genre4, .genre5").fadeOut(250);
     })
     $(".genre>button:nth-of-type(4)").click(function () {
         $(this).addClass("btn_on");
         $(".genre>button:nth-of-type(1), .genre>button:nth-of-type(2), .genre>button:nth-of-type(3), .genre>button:nth-of-type(5)").removeClass("btn_on");
-        $(".genre4").fadeIn(250);
+        $(".genre4").fadeIn(250).css("zIndex", "1");
         $(".genre1, .genre2, .genre3, .genre5").fadeOut(250);
     })
     $(".genre>button:nth-of-type(5)").click(function () {
         $(this).addClass("btn_on");
         $(".genre>button:nth-of-type(1), .genre>button:nth-of-type(2), .genre>button:nth-of-type(3), .genre>button:nth-of-type(4)").removeClass("btn_on");
-        $(".genre5").fadeIn(250);
+        $(".genre5").fadeIn(250).css("zIndex", "1");
         $(".genre1, .genre2, .genre3, .genre4").fadeOut(250);
     })
+    //qna
+    $('.qna1').click(function () {
+        $('.q1').toggleClass('q_on');
+        $('.a1').slideToggle(250);
+    })
+    $('.qna2').click(function () {
+        $('.q2').toggleClass('q_on');
+        $('.a2').slideToggle(250);
+    })
+    $('.qna3').click(function () {
+        $('.q3').toggleClass('q_on');
+        $('.a3').slideToggle(250);
+    })
+    $('.qna4').click(function () {
+        $('.q4').toggleClass('q_on');
+        $('.a4').slideToggle(250);
+    })
+    //footer_info
+    $('.footer_info button').click(function () {
+        $(this).prop('value', 1)
+        $('.footer_info ul').toggleClass('info_onoff');
+    })
 });
+
+// if
+// value = 1;
+// button text = 펼쳐보기,
+// else
+// value = 2;
+// button text = 닫기.
