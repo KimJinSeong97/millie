a50c127ead29344518ef177645d58ccb49d94a1b
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-07-20 19:50:11 +0900

    220720
    millie index.html finished!!

--- js/index.js
+++ js/index.js
@@ -195,14 +195,138 @@ $(function () {
     })
     //footer_info
     $('.footer_info button').click(function () {
-        $(this).prop('value', 1)
+        if ($(this).val() == 1) {
+            $(this).html("사업자 정보 닫기");
+            $(this).prop("value", 2);
+        } else {
+            $(this).html("사업자 정보 펼쳐보기");
+            $(this).attr("value", 1);
+        }
         $('.footer_info ul').toggleClass('info_onoff');
     })
 });
 
-// if
-// value = 1;
-// button text = 펼쳐보기,
-// else
-// value = 2;
-// button text = 닫기.
+//tap click
+document.querySelector('.tap1').addEventListener('click', function () {
+    this.classList.add('tap_on');
+    document.querySelector('.tap2').classList.remove('tap_on');
+    document.querySelector('.tap3').classList.remove('tap_on');
+    document.querySelector('.cw_1').style.display = "block";
+    document.querySelector('.cw_2').style.display = "none";
+    document.querySelector('.cw_3').style.display = "none";
+});
+document.querySelector('.tap2').addEventListener('click', function () {
+    this.classList.add('tap_on');
+    document.querySelector('.tap1').classList.remove('tap_on');
+    document.querySelector('.tap3').classList.remove('tap_on');
+    document.querySelector('.cw_2').style.display = "block";
+    document.querySelector('.cw_1').style.display = "none";
+    document.querySelector('.cw_3').style.display = "none";
+});
+document.querySelector('.tap3').addEventListener('click', function () {
+    this.classList.add('tap_on');
+    document.querySelector('.tap1').classList.remove('tap_on');
+    document.querySelector('.tap2').classList.remove('tap_on');
+    document.querySelector('.cw_3').style.display = "block";
+    document.querySelector('.cw_1').style.display = "none";
+    document.querySelector('.cw_2').style.display = "none";
+});
+
+//phone screen slide contents_wrap1
+document.querySelector('.cw_1 .screen_btn1').addEventListener('click', function () {
+    this.classList.add('btn_on');
+    document.querySelector('.cw_1 .screen_btn2').classList.remove('btn_on');
+    document.querySelector('.cw_1 .screen_btn3').classList.remove('btn_on');
+    document.querySelector('.cw_1 .screen_btn4').classList.remove('btn_on');
+    document.querySelector('.cw_1 .phone_wrap').style.transform = 'translate(-0px)';
+    document.querySelector('.cw_1 .ment').innerHTML = "<b>하루의 시작과 마무리를 오디오북과 함께</b> <br> 지하철에서도, 잠들기 전에도 독서하세요";
+});
+
+document.querySelector('.cw_1 .screen_btn2').addEventListener('click', function () {
+    this.classList.add('btn_on');
+    document.querySelector('.cw_1 .screen_btn1').classList.remove('btn_on');
+    document.querySelector('.cw_1 .screen_btn3').classList.remove('btn_on');
+    document.querySelector('.cw_1 .screen_btn4').classList.remove('btn_on');
+    document.querySelector('.cw_1 .phone_wrap').style.transform = 'translate(-330px)';
+    document.querySelector('.cw_1 .ment').innerHTML = "<b>귀로 듣다가 눈으로도 같이 보면</b> <br> 더 내용이 잘 이해돼요";
+});
+
+document.querySelector('.cw_1 .screen_btn3').addEventListener('click', function () {
+    this.classList.add('btn_on');
+    document.querySelector('.cw_1 .screen_btn1').classList.remove('btn_on');
+    document.querySelector('.cw_1 .screen_btn2').classList.remove('btn_on');
+    document.querySelector('.cw_1 .screen_btn4').classList.remove('btn_on');
+    document.querySelector('.cw_1 .phone_wrap').style.transform = 'translate(-660px)';
+    document.querySelector('.cw_1 .ment').innerHTML = "<b>30분만에 책 요약</b> <br> 핵심만 쏙 골라 읽어주는 오디오북";
+});
+
+document.querySelector('.cw_1 .screen_btn4').addEventListener('click', function () {
+    this.classList.add('btn_on');
+    document.querySelector('.cw_1 .screen_btn1').classList.remove('btn_on');
+    document.querySelector('.cw_1 .screen_btn2').classList.remove('btn_on');
+    document.querySelector('.cw_1 .screen_btn3').classList.remove('btn_on');
+    document.querySelector('.cw_1 .phone_wrap').style.transform = 'translate(-990px)';
+    document.querySelector('.cw_1 .ment').innerHTML = "<b>이제훈부터 옥주현까지</b> <br> 셀럽이 읽어주는 오디오북";
+});
+
+//phone screen slide contents_wrap2
+document.querySelector('.cw_2 .screen_btn1').addEventListener('click', function () {
+    this.classList.add('btn_on');
+    document.querySelector('.cw_2 .screen_btn2').classList.remove('btn_on');
+    document.querySelector('.cw_2 .screen_btn3').classList.remove('btn_on');
+    document.querySelector('.cw_2 .screen_btn4').classList.remove('btn_on');
+    document.querySelector('.cw_2 .phone_wrap').style.transform = 'translate(-0px)';
+    document.querySelector('.cw_2 .ment').innerHTML = "<b>시선 추적 기능</b> <br> 눈동자로 페이지를 넘길 수 있어요";
+});
+
+document.querySelector('.cw_2 .screen_btn2').addEventListener('click', function () {
+    this.classList.add('btn_on');
+    document.querySelector('.cw_2 .screen_btn1').classList.remove('btn_on');
+    document.querySelector('.cw_2 .screen_btn3').classList.remove('btn_on');
+    document.querySelector('.cw_2 .screen_btn4').classList.remove('btn_on');
+    document.querySelector('.cw_2 .phone_wrap').style.transform = 'translate(-330px)';
+    document.querySelector('.cw_2 .ment').innerHTML = "<b>어느 기기에서도 OK</b> <br> 스마트폰, 태블릿, PC, E ink 기기까지";
+});
+
+document.querySelector('.cw_2 .screen_btn3').addEventListener('click', function () {
+    this.classList.add('btn_on');
+    document.querySelector('.cw_2 .screen_btn1').classList.remove('btn_on');
+    document.querySelector('.cw_2 .screen_btn2').classList.remove('btn_on');
+    document.querySelector('.cw_2 .screen_btn4').classList.remove('btn_on');
+    document.querySelector('.cw_2 .phone_wrap').style.transform = 'translate(-660px)';
+    document.querySelector('.cw_2 .ment').innerHTML = "<b>다크 모드</b> <br> 전자책이 눈 아프다는 편견은 버려요";
+});
+
+document.querySelector('.cw_2 .screen_btn4').addEventListener('click', function () {
+    this.classList.add('btn_on');
+    document.querySelector('.cw_2 .screen_btn1').classList.remove('btn_on');
+    document.querySelector('.cw_2 .screen_btn2').classList.remove('btn_on');
+    document.querySelector('.cw_2 .screen_btn3').classList.remove('btn_on');
+    document.querySelector('.cw_2 .phone_wrap').style.transform = 'translate(-990px)';
+    document.querySelector('.cw_2 .ment').innerHTML = "<b>자유로운 보기 설정</b> <br> 줄간격부터 폰트까지 내 마음대로 설정하면 <br> 더욱 편한 독서를 할 수 있어요";
+});
+
+//phone screen slide contents_wrap3
+document.querySelector('.cw_3 .screen_btn1').addEventListener('click', function () {
+    this.classList.add('btn_on');
+    document.querySelector('.cw_3 .screen_btn2').classList.remove('btn_on');
+    document.querySelector('.cw_3 .screen_btn3').classList.remove('btn_on');
+    document.querySelector('.cw_3 .phone_wrap').style.transform = 'translate(-0px)';
+    document.querySelector('.cw_3 .ment').innerHTML = "<b>당신을 위한 추천</b> <br> 키워드로 한 눈에 정리한 추천 도서";
+});
+
+document.querySelector('.cw_3 .screen_btn2').addEventListener('click', function () {
+    this.classList.add('btn_on');
+    document.querySelector('.cw_3 .screen_btn1').classList.remove('btn_on');
+    document.querySelector('.cw_3 .screen_btn3').classList.remove('btn_on');
+    document.querySelector('.cw_3 .phone_wrap').style.transform = 'translate(-330px)';
+    document.querySelector('.cw_3 .ment').innerHTML = "<b>이럴 땐 이런 책</b> <br> 내 상황에 딱 맞는 회원들의 추천 책";
+});
+
+document.querySelector('.cw_3 .screen_btn3').addEventListener('click', function () {
+    this.classList.add('btn_on');
+    document.querySelector('.cw_3 .screen_btn1').classList.remove('btn_on');
+    document.querySelector('.cw_3 .screen_btn2').classList.remove('btn_on');
+    document.querySelector('.cw_3 .phone_wrap').style.transform = 'translate(-660px)';
+    document.querySelector('.cw_3 .ment').innerHTML = "<b>책-잇</b> <br> 책을 통해 트렌드와 문해력을 한번에!";
+});
