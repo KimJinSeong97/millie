1ddd98e69d528cb2e9dc8b674ee251473949ff5b
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-07-26 15:04:21 +0900

    220726
    millie finished..

--- js/sub.js
+++ js/sub.js
@@ -44,3 +44,71 @@ $.ajax({
             $("ul > .list").eq(i).append("<h5>" + msg.documents[i].publisher + "</h5>");
         }
     });
+
+$(function () {
+    $('.tap1').click(function () {
+        $(this).addClass("tap_on");
+        $('.tap2').removeClass("tap_on");
+        $('.tap3').removeClass("tap_on");
+        $('.contents1').css("display", "block");
+        $('.contents2, .contents3').css("display", "none");
+    })
+    $('.tap2').click(function () {
+        $(this).addClass("tap_on");
+        $('.tap1').removeClass("tap_on");
+        $('.tap3').removeClass("tap_on");
+        $('.contents2').css("display", "block");
+        $('.contents1, .contents3').css("display", "none");
+    })
+    $('.tap3').click(function () {
+        $(this).addClass("tap_on");
+        $('.tap1').removeClass("tap_on");
+        $('.tap2').removeClass("tap_on");
+        $('.contents3').css("display", "block");
+        $('.contents1, .contents2').css("display", "none");
+    })
+})
+
+// scroll logo change
+$(window).scroll(function () {
+    var ws = $(this).scrollTop();
+    var scroll_nav = $('.tap_menu').offset().top;
+    if (ws > scroll_nav - 10) {
+        $('nav img').attr("src", "img/index/simple-logo.png").css("width", "23px");
+    } else {
+        $('nav img').attr("src", "img/index/full-logo.png").css("width", "56px");;
+    }
+})
+
+//footer_info
+$('.footer_info button').click(function () {
+    if ($(this).val() == 1) {
+        $(this).html("사업자 정보 닫기");
+        $(this).prop("value", 2);
+    } else {
+        $(this).html("사업자 정보 펼쳐보기");
+        $(this).attr("value", 1);
+    }
+    $('.footer_info ul').toggleClass('info_onoff');
+})
+
+//slide
+$(function () {
+    var page = 0;
+    $('.prev').click(function () {
+        page--;
+        if (page < 0) {
+            page = 0;
+            return;
+        }
+        $('.slide').stop().animate({ marginLeft: -200 * page }, 250);
+    });
+    $('.next').click(function () {
+        page++;
+        if (page > $('.slide .list').length - 6) {
+            page = $('.slide .list').length - 6;
+            return;
+        }
+        $('.slide').stop().animate({ marginLeft: -200 * page }, 250);
+    })
+})
